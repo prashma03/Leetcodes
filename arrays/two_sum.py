79602e63e225d79709ef28286cf3fa2523b07d97
Pronita Ghimire <ghimireprashma2006@gmail.com>
2026-03-30 21:21:31 -0500

    Update two_sum.py
    ## Two Sum
    
    **Approach:**
    - Used hashmap to store visited numbers
    - For each element, check if complement exists
    
    **Time Complexity:** O(n)  
    **Space Complexity:** O(n)

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -1,1 +1,14 @@
-
+#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashmap = {}
+        for i, num in enumerate(nums): 
+          diff = target - num 
+          if diff in hashmap:
+            return [hashmap[diff], i] 
+          hashmap[num] = i
