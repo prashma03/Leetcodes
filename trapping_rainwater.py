6e3c8be0721426b1c5c9d8c43f0c4c682e9a3662
Pronita Ghimire <ghimireprashma2006@gmail.com>
2026-03-31 22:27:00 -0500

    Update trapping_rainwater.py

--- trapping_rainwater.py
+++ trapping_rainwater.py
@@ -1,3 +1,8 @@
 #Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
-
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
  
