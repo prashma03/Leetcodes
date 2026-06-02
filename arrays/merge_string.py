6d8c7410bd7b960163c25f868e1f7a1897bb932a
Pronita Ghimire <ghimireprashma2006@gmail.com>
2026-06-02 10:58:27 -0500

    Implement merge_string function for alternating merge
    Added a function to merge two strings in alternating order.

--- arrays/merge_string.py
+++ arrays/merge_string.py
@@ -1,1 +1,21 @@
-
+# You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+# Return the merged string.
+# Example 1:
+# Input: word1 = "abc", word2 = "pqr"
+# Output: "apbqcr"
+# Explanation: The merged string will be merged as so:
+# word1:  a   b   c
+# word2:    p   q   r
+# merged: a p b q c r
+def merge_string(word1: str, word2: str)->str:
+  result = ""
+  i=0
+  j=0
+  while i<len(word1) and j<len(word2):
+    result += word1[i]
+    result += word2[j]
+    i+1
+    j+1
+  result += word1[i:]
+  result += word2[j:]
+  return result
