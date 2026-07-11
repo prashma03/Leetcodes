e3db9dcc3b8c6fcac4fbe509fbd13fda3e17fed9
Pronita Ghimire <ghimireprashma2006@gmail.com>
2026-07-10 23:21:09 -0500

    Add merge function for merging two sorted arrays
    Implemented a merge function to combine two sorted arrays in-place.

--- Medium/Gray_Code.py
+++ Medium/Gray_Code.py
@@ -1,1 +1,29 @@
+def merge(nums1, m, nums2, n):
+    i = m - 1          # Last valid element in nums1
+    j = n - 1          # Last element in nums2
+    k = m + n - 1      # Last position in nums1
 
+    while i >= 0 and j >= 0:
+        if nums1[i] > nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
+        else:
+            nums1[k] = nums2[j]
+            j -= 1
+        k -= 1
+
+    # Copy remaining elements from nums2 (if any)
+    while j >= 0:
+        nums1[k] = nums2[j]
+        j -= 1
+        k -= 1
+
+
+# Example
+nums1 = [1, 2, 3, 0, 0, 0]
+m = 3
+nums2 = [2, 5, 6]
+n = 3
+
+merge(nums1, m, nums2, n)
+print(nums1)
