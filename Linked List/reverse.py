8c08277eb9e9f985c8b0ed2ea3074c9f68e81c88
Pronita Ghimire <ghimireprashma2006@gmail.com>
2026-07-14 11:48:08 -0500

    Implement function to reverse a linked list

--- Linked List/reverse.py
+++ Linked List/reverse.py
@@ -1,1 +1,11 @@
+def reverseList(head):
+    prev = None
+    current = head
 
+    while current:
+        next_node = current.next   # Save where to go next
+        current.next = prev        # Reverse the arrow
+        prev = current             # Move prev forward
+        current = next_node        # Continue with saved node
+
+    return prev
