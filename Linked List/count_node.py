66137fdb1ae4745a9e95f3b3d40508a84f87b0fb
Pronita Ghimire <ghimireprashma2006@gmail.com>
2026-07-16 10:36:01 -0500

    Add ListNode class and count_nodes function

--- Linked List/count_node.py
+++ Linked List/count_node.py
@@ -1,1 +1,14 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
+
+def count_nodes(head):
+    count = 0
+
+    while head:
+        count += 1
+        head = head.next
+
+    return count
