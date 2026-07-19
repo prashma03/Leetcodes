5868895017c7f01114b58ad0d9da95172fe9fc61
Pronita Ghimire <ghimireprashma2006@gmail.com>
2026-07-19 18:38:58 -0500

    Add function to group anagrams from a list

--- strings/group_amalgam.py
+++ strings/group_amalgam.py
@@ -1,1 +1,7 @@
-
+from collections import defaultdict
+def groupAnalgams(strs):
+  groups = defaultdict(list)
+  for word in strs:
+    key = "".join(sorted(word))
+    groups[key].append(word)
+  return list(groups.values())
