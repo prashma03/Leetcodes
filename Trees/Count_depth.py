35a707532ec1fdecae7527602bd28e33be33ed2a
Pronita Ghimire <ghimireprashma2006@gmail.com>
2026-07-20 10:13:54 -0500

    Add count_depth method to Solution class

--- Trees/Count_depth.py
+++ Trees/Count_depth.py
@@ -1,1 +1,8 @@
-
+class Solution:
+  def count_depth(self, root):
+    if not root:
+      return 0
+    left = self.count_depth(root.left)
+    right = self.count_depth(root.right)
+    return 1 + max(left,right)
+      
