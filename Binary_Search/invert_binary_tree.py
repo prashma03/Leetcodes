247afb07e599d5ed759abba33b53067d851ba51a
Pronita Ghimire <ghimireprashma2006@gmail.com>
2026-07-20 09:48:39 -0500

    Implement invertTree function for binary tree inversion
    Fix indentation and recursion in invertTree function.

--- Binary_Search/invert_binary_tree.py
+++ Binary_Search/invert_binary_tree.py
@@ -1,1 +1,7 @@
-
+def invertTree(root):
+  if not root:
+    return None
+    root.left, root.right = root.right, root.left
+    self.invertTree(root.Left)
+    self.invertTree(root.right)
+    return root
